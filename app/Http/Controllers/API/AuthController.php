22164de7290a076450b66b2892771d05d092c3bb
Isaac Oduh <ufuomaoduh@gmail.com>
2020-12-11 10:06:04 +0100

    Basic Authentication Setup

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,51 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $loginData = $request->only(['email','password']);
+        if(!$token = auth()->attempt($loginData)){
+            return response()->json(['message' => 'Invalid Credentials'], 400);
+        }
+
+        $user = User::where('email', auth()->user()->email)->first();
+        $user->last_login = now('UTC');
+        $user->save();
+        return response()->json(['token' => $token, 'user' => auth()->user()]);
+    }
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'bail|required',
+            'email' => 'bail|required|email|unique:users',
+            'password' => 'bail|required',
+            'role' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->messages(),400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        $user->save();
+        return response()->json(['message' => 'Sign up successful']);
+    }
+
+    public function getAllUsers(Request $request){
+        $users = User::with('orders')->get();
+        return response()->json(['data' => $user]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::namespace('API')->group(function(){
+    Route::prefix('auth')->group(function(){
+        Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register'])->name('signup');
+        Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login'])->name('signin');
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
